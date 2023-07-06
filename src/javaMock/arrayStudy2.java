159a6c175ff4e1c942b22cc5ba4100092ded5254
rupeshWathore <rupeshwathore@gmail.com>
2023-07-06 15:41:38 +0530

    [rupeshWathore] adding project to git

---
+++ src/javaMock/arrayStudy2.java
@@ -0,0 +1,26 @@
+package javaMock;
+
+public class arrayStudy2 {
+
+	public static void main(String[] args) 
+	{
+		
+		// single dimensional - without using new keyword
+		char division[]={'A','B','C'};     // capacity=length=3
+		System.out.println(division);
+		System.out.println(division.length);
+		
+		System.out.println(division[0]);
+		
+		
+		for(int i=0;i<=division.length-1;i++)
+		{
+			System.out.println(division[i]);
+		}
+		
+		
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rupes
+ *
+ */
+module mock {
+}
